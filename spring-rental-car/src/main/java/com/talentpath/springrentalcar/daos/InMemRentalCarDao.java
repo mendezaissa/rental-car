88f1d22f2f78fa55177f606bcdbe90c5d586cc50
Aissa Mendez <amendez@C02CM97UML7H.local>
2020-12-08 17:00:39 -0600

    getAllCars servicesTest

---
+++ spring-rental-car/src/main/java/com/talentpath/springrentalcar/daos/InMemRentalCarDao.java
@@ -0,0 +1,93 @@
+package com.talentpath.springrentalcar.daos;
+
+import com.talentpath.springrentalcar.exceptions.BookingDaoException;
+import com.talentpath.springrentalcar.exceptions.NoCarFoundException;
+import com.talentpath.springrentalcar.exceptions.NoTransactionFoundException;
+import com.talentpath.springrentalcar.models.Book;
+import com.talentpath.springrentalcar.models.Transaction;
+import com.talentpath.springrentalcar.models.Car;
+import com.talentpath.springrentalcar.models.Transaction;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@Profile("servicetesting")
+public class InMemRentalCarDao implements CarDao{
+
+    List<Car> allCars;
+    List<Transaction> allTransactions;
+
+    public InMemRentalCarDao(){
+        reset();
+    }
+
+    @Override
+    public void reset() {
+        allCars = new ArrayList<>();
+        allCars.clear();
+
+        Car car1 = new Car();
+        car1.setCarId(1);
+        car1.setMake("Audi");
+        car1.setModel("R8");
+        car1.setRate(800);
+
+        allCars.add(car1);
+
+        allTransactions = new ArrayList<>();
+        allTransactions.clear();
+
+        Transaction booking1 = new Transaction();
+        booking1.setTransactionId(1);
+        booking1.setCarId(1);
+        booking1.setCustomerId(1);
+
+        Date from = new Date();
+        booking1.setFrom( from );
+
+        Date to = new Date();
+        booking1.setTo( to );
+
+        allTransactions.add(booking1);
+    }
+
+    @Override
+    public List<Car> getAllCars() {
+        return allCars.stream().map( toCopy -> new Car( toCopy ) ).collect( Collectors.toList() );
+    }
+
+    @Override
+    public Car getById(Integer carId) throws NoCarFoundException {
+        return null;
+    }
+
+    @Override
+    public List<Transaction> getCarBookings(Integer carId) throws NoCarFoundException {
+        return null;
+    }
+
+    @Override
+    public void addTransaction(Book toBook) throws BookingDaoException {
+
+    }
+
+    @Override
+    public List<Transaction> getAllTransactions() {
+        return null;
+    }
+
+    @Override
+    public void deleteByTransactionId(Integer id) throws NoTransactionFoundException {
+
+    }
+
+    @Override
+    public void updateTransaction(Transaction transaction) {
+
+    }
+}
---
+++ spring-rental-car/src/test/java/com/talentpath/springrentalcar/services/RentalCarServiceTest.java
@@ -0,0 +1,42 @@
+package com.talentpath.springrentalcar.services;
+
+import com.talentpath.springrentalcar.daos.CarDao;
+import com.talentpath.springrentalcar.daos.InMemRentalCarDao;
+import com.talentpath.springrentalcar.models.Car;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("servicetesting")
+public class RentalCarServiceTest {
+
+    @Autowired
+    CarService serviceToTest;
+
+    @Autowired
+    CarDao dao;
+
+    @BeforeEach
+    void setUp(){
+        dao = new InMemRentalCarDao();
+        serviceToTest = new CarService(dao);
+        dao.reset();
+    }
+
+    @Test
+    void getAllCars(){
+        List<Car> allCars = serviceToTest.getCars();
+        assertEquals( 1, allCars.size() );
+    }
+
+}
